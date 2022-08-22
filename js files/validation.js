a368442f00452be3b6555f2559db41f26ee2313d
Azharul-Islam-Nahid <lonewolfazharul@gmail.com>
2022-08-22 17:39:27 +0600

    added js for validation

---
+++ js files/validation.js
@@ -0,0 +1,23 @@
+function handleOnclick() {
+   const inputValue = document.getElementById('inputfield').value;
+   if (inputValue === '') {
+      return alert('Field is empty input valid digits');
+   }
+}
+
+
+function handleOnclick() {
+   const inputValue = document.getElementById('inputfield-2').value;
+   if (inputValue === '') {
+      return alert('Field is empty input valid digits');
+   }
+
+}
+
+function handleOnclick() {
+   const inputValue = document.getElementById('inputfield-3').value;
+   if (inputValue === '') {
+      return alert('Field is empty input valid digits');
+   }
+
+}
